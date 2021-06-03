50adf3fd45d0654911e66839e33d0d1dfeb9c402
makiahdallas <76473945+makiahdallas@users.noreply.github.com>
2021-06-03 17:25:28 -0500

    Update candidate-testing.js
    all questions are asked, and answers are returned

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,59 +1,77 @@
 const input = require('readline-sync');
 
-// TODO 2: modify your quiz app to ask 5 questions //
+//TODO 2: modify your quiz app to ask 5 questions
 
 // TODO 1.1a: Define candidateName // 
-let candidateName="";
+let candidateName= "";
 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question="Who was the first American woman in space?";
 let correctAnswer="Sally Ride";
-let candidateAnswer="";
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let candidateAnswer;
+let questions = [
+"Who was the first American woman in space? ", 
+"True or false: 5 kilometer == 5000 meters? ",
+"(5 + 3)/2 * 10 = ",
+"Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 
+"What is the minimum crew size for the ISS? "
+];
+
+let correctAnswers = ["sally ride",
+ "true", 
+ "40",
+ "trajectory",
+ "3"
+];
+
+let candidateAnswers = [""];
 
 function askForName() {
-  // TODO 1.1b: Ask for candidate's name //
- candidateName = input.question("What is your name?")
+// TODO 1.1b: Ask for candidate's name //
+candidateName = input.question("What is your name? ");
+console.log(`Hello, ${candidateName}! Welcome to your quiz.\nPlease answer the following questions:`);
+console.log("----------------------");
 }
 
 function askQuestion() {
-  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-candidateAnswer = input.question(question);
+// TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
+for (let i=0; i<questions.length; i++) {
+//input.question(questions[i]); 
+candidateAnswers[i] = input.question(questions[i]);
+if (candidateAnswers[i] === correctAnswers[i]) {
+console.log(`${candidateAnswers[i]} is correct!`);
+} else { 
+console.log(`Your Answer: ${candidateAnswers[i]} is not correct. Correct Answer: ${correctAnswers[i]}`); 
+console.log("--------------------------");}
 }
+}
+function gradeQuiz() {
+  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly
+console.log(`Your quiz is now complete.\nThank you, ${candidateName}. Here are your results:`);
 
-function gradeQuiz(candidateAnswers) {
+let grade=""; 
 
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-console.log(candidateAnswer);
-if (candidateAnswer === correctAnswer) {
-  console.log("That is correct!");
-} else 
- {
-  console.log("Sorry, that is not correct.");
-  } 
-
-  let grade;
-  
-
-  return grade;
+//if (grade < 60) {console.log("Sorry, you did not pass the quiz. Try again.); } 
+//else if (grade < 70) { 
+//console.log("You passed. Good job."); 
+//} else if (grade <= 100) {
+//console.log("Wow! You got a 100%! Great Job."); 
+//}
+return grade;
 }
 
 function runProgram() {
-  askForName();
-  // TODO 1.1c: Ask for candidate's name //
-  console.log(`Hello, ${candidateName}!`);
   
+  // TODO 1.1c: Ask for candidate's name //
+  askForName();
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
 
 // Don't write any code below this line //
-// And don't change these or your program will not run as expected //
+// And don't change these or your program will not run as expected
 module.exports = {
-  candidateName: candidateName,
+candidateName: candidateName,
   question: question,
   correctAnswer: correctAnswer,
   candidateAnswer: candidateAnswer,
